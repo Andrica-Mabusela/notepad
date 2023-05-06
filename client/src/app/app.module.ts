e9212cd703efa90ddb78c1790f3e760fd4e32aa8
Andrica Mabusela <programmerandy420@gmail.com>
2023-05-06 14:59:09 +0200

    initial

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'login', component: LoginComponent,
+    },
+  {
+    path: 'login', component: RegisterComponent,
+  }]),
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
